ed4f32e750e5aab0e685959f6b99470cfa3282b7
Christian Solano <christiansolano@Christians-MacBook-Air-2.local>
2024-04-14 16:40:40 -0700

    While loop

--- pipe.c
+++ pipe.c
@@ -3,33 +3,41 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
-
 int main(int argc, char *argv[]) {
-    printf("hello world (pid:%d)\n", (int) getpid());
     int command_n = 1;
-    char *command1 = argv[1];
-    //command 1 printing 
-    fprintf(stderr, "command1: %s\n", command1);
-    int fork_rv = fork(); //return value: -1 if failure, 0 in child, PID in parent
-    //handle error
-    if (fork_rv < 0){
-        fprintf(stderr, "fork failed\n");
-        exit(1);
-    } else if (fork_rv == 0) {
-        printf("hello, I am child (pid:%d)\n", (int) getpid());
-        //handle child
-        printf("I am the child");
-        execlp(command1, command1, NULL);
-        //shouldn't be outputted, checking for errors
-        printf("this shouldn't print");
-    } else {
-        printf("hello, I am parent(pid:%d)\n", (int) getpid());
-        //parent
-        //wait for child to finish
-        int wait_rv = wait(NULL);
-        if (wait_rv == -1) {
-            printf("wait failed\n");
+    char *command_s = NULL;
+    int pipefd[2]; // pipe file descriptors
+    while (command_n < argc) {
+        command_s = argv[command_n];
+        fprintf(stderr, "commandn: %d\n", command_n);
+        fprintf(stderr, "commands: %s\n", command_s);
+        int pipe_val = pipe(pipefd); // create pipe
+        if (pipe_val == -1) {
+            fprintf(stderr, "pipe failed\n");
+            exit(1);
         }
-    }
+        int fork_rv = fork(); // return value: -1 if failure, 0 in child,
+        // PID in parent
+        // handle error
+        if (fork_rv < 0) {
+            fprintf(stderr, "fork failed\n");
+            exit(1);
+        } else if (fork_rv == 0) {
+            // handle child
+            printf("I am the child");
+            close(pipefd[1]); // close write end
+            execlp(command_s, command_s, NULL); // shouldn't be outputted, checking
+            // nothing should print
+        } else {
+            printf("hello, I am parent(pid:%d)\n", (int)getpid());
+            close(pipefd[0]); //close read end
+            // wait for child to finish
+            int wait_rv = wait(NULL);
+            if (wait_rv == -1) {
+                printf("wait failed\n");
+            }
+        }
+        command_n++;
+    } // command 1 printing
     return 0;
 }
