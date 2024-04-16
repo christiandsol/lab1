aeb9d4abc3e9d15c00966e5a59dee29d2d5e71de
Christian Solano <christiansolano@Christians-MacBook-Air-2.local>
2024-04-15 18:20:44 -0700

    Test bogus

--- pipe.c
+++ pipe.c
@@ -12,23 +12,28 @@ int main(int argc, char *argv[]) {
     ssize_t bytes_read;
     if (pipe(pipefd[0]) < 0){ //create 1st pipe
         fprintf(stderr, "pipe error");
-        return 1;
+        exit(1);
     }
     int rv = fork(); //1st child
     if (rv < 0) {
         fprintf(stderr, "fork failed\n");
-        return 1;
+        exit(1);
     } else if (rv == 0) {//child
         close(pipefd[0][0]); //close read end
         dup2(pipefd[0][1],STDOUT_FILENO); //redirect stdout to write end of pipe 
         if (execlp(argv[1], argv[1], NULL) < 0){
             fprintf(stderr, "bogus argument");
-            return 1;
+            exit(1);
         }
     } else { //parent
         close(pipefd[0][1]); //close write end
         dup2(pipefd[0][0], STDIN_FILENO); //redirect stdin to read end of pipe
-        wait(NULL); //wait for child
+        int status;
+        wait(&status); //wait for child
+        if (WEXITSTATUS(status) == 1) {
+            fprintf(stderr, "child exited with status 1\n");
+            exit(1);
+        }
     }
 
 
@@ -36,12 +41,12 @@ int main(int argc, char *argv[]) {
     for (int i = 1; i < argc - 2; i++) {
         if (pipe(pipefd[i]) < 0){ //create ith pipe
             fprintf(stderr, "pipe error");
-            return 1;
+            exit(1);
         }
         rv = fork();
         if (rv < 0) {
             fprintf(stderr, "fork failed\n");
-            return 1;
+            exit(1);
         } else if (rv == 0) { //child
             //data is in read end of pipe i-1
             close(pipefd[i][0]); //close read end
@@ -49,36 +54,48 @@ int main(int argc, char *argv[]) {
             dup2(pipefd[i][1],STDOUT_FILENO); //redirect stdout to write end of pipe 
             if (execlp(argv[i + 1], argv[i + 1], NULL) < 0){
                 fprintf(stderr, "bogus argument");
-                return 1;
+                exit(1);
             }
         } else { //parent
             close(pipefd[i][1]); //close write end
             dup2(pipefd[i][0], STDIN_FILENO); //redirect stdin to read end of pipe
             wait(NULL); //wait for child
+            int status;
+            wait(&status); //wait for child
+            if (WEXITSTATUS(status) == 1) {
+                fprintf(stderr, "child exited with status 1\n");
+                exit(1);
+            }
         }
     }
 
     //parent last program
     if (pipe(pipefd[argc - 2]) < 0){ //create ith pipe
         fprintf(stderr, "pipe error");
-        return 1;
+        exit(1);
     } 
     rv = fork();
     if (rv < 0) {
         fprintf(stderr, "fork failed\n");
-        return 1;
+        exit(1);
     } else if (rv == 0) { //child
         // redirect previous pipe output to stdin
         close(pipefd[argc - 2][0]); //close read end
         dup2(pipefd[argc - 3][1], pipefd[argc - 2][0]);
         if(execlp(argv[argc - 1], argv[argc - 1], NULL) < 0){
             fprintf(stderr, "bogus argument");
-            return 1;
+            exit(1);
         }
     } else { //parent
         close(pipefd[argc - 2][1]); //close write end
         close(pipefd[argc - 2][0]); //close read end
         wait(NULL); //wait for child
+        int status;
+        wait(&status); //wait for child
+        if (WEXITSTATUS(status) == 1) {
+            fprintf(stderr, "child exited with status 1\n");
+            exit(1);
+        }
     }
 
     bytes_read = read(pipefd[argc -  2][0], &buffer, sizeof(buffer));
